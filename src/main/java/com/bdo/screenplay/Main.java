a4ec36158acf6188d6ed836fbb5be1b2a9c0510a
jesus.ramirez <jesus1.ramirez@ucp.edu.co>
2025-05-19 09:21:29 -0500

    Se agrego funcionalidad

--- src/main/java/com/bdo/screenplay/Main.java
+++ src/main/java/com/bdo/screenplay/Main.java
@@ -4,6 +4,6 @@ package com.bdo.screenplay;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Javabank ATM!");
+        System.out.println("Welcome to Javabank ATM! Version control with Git.");
     }
 }
