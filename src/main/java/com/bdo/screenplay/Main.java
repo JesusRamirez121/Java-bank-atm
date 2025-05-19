842afde798ca8bd330e82e7501f1719971227f40
jesus.ramirez <jesus1.ramirez@ucp.edu.co>
2025-05-19 10:06:01 -0500

    variables y operadores en java para cajero automatico

--- src/main/java/com/bdo/screenplay/Main.java
+++ src/main/java/com/bdo/screenplay/Main.java
@@ -4,6 +4,25 @@ package com.bdo.screenplay;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Javabank ATM! Version control with Git.");
+        //variables basicas
+        String accountNumber = "123456789";
+        double balance = 1500.75;
+        int pin = 1234;
+
+        // Array de montos de transacciones
+        int[] transactionAmounts = {200, -100, 50};
+
+        //operaciones con variables
+        balance += transactionAmounts[0]; // Deposito
+        if (balance>0 && pin == 1234) {
+            System.out.println("Transaction successful. New balance: " + balance);
+        } else {
+            System.out.println("Transaction failed. Insufficient funds or incorrect PIN.");
+        }
+
+        // uso de operadores
+        balance++; // Incrementa el saldo
+        String status = (balance < 0) ?"Deuda" : "Credito";
+        System.out.println("Account status: " + status);
     }
 }
